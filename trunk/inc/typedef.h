f93d68ba2f610ffe04ee1839d667ad4d30f9d7fd
williams.matias <williams.matias@e873271c-9239-11de-a307-bbee0a36ac66>
2009-08-28 21:04:39 +0000

    Agregue el typedef.h donde pueden poner todos los typedef

---
+++ trunk/inc/typedef.h
@@ -0,0 +1,16 @@
+/*
+ * Header que contiene los typedefs a utilizar en el programa.
+ */ 
+
+#ifndef _TYPEDEF_H
+#define _TYPEDEF_H
+
+typedef unsigned char boolean;
+
+typedef struct point_t {
+
+    int x;
+    int y;
+} point_t;
+
+#endif
