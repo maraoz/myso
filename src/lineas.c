792b23fad148e693b98955ccd3dd0271dec1e64c
williams.matias <williams.matias@e873271c-9239-11de-a307-bbee0a36ac66>
2009-09-02 18:56:05 +0000

    Agregando nuevas funciones para que funcoine con threads

---
+++ src/lineas.c
@@ -0,0 +1,46 @@
+#include <pthread.h>
+#include "../inc/typedef.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct {
+    point_t * path;
+    point_t * stops;
+    int path_length;
+    int stops_length;
+} buses_line;
+
+bus_line buses;
+
+int
+main(void){
+
+    int qty_buses;
+    pthread_t * buses_threads;
+    int aux_pthread_creation;
+
+
+    buses.path_length = get_path(0, &buses.path);
+    qty_buses = get_qty_buses(0);
+    buses_threads = malloc(sizeof(pthread_t) * qty_buses);
+    if(buses_threads == NULL){
+	printf("No hay suficiente memoria \n");
+	return 1;
+    }
+    buses.stops_length = get_stops(0, &buses.stops);
+
+    while(qty_buses > 0) {
+	aux_pthread_creation = pthread_create(&threads[qty_buses], NULL, new_bus, (void *)t);
+	if(!rc){
+	    qty_buses--;
+	} else {
+	    printf("No se puedo crear el colectivo \n");
+	}
+    }
+    
+}
+
+void *
+new_bus() {
+    
+}
