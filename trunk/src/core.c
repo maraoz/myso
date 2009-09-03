91e114aff0dd9f1d88982ed8585980762a7ba5bb
williams.matias <williams.matias@e873271c-9239-11de-a307-bbee0a36ac66>
2009-09-03 21:19:09 +0000

    Algunos progresos sobre los semaforos en la ciudad

--- trunk/src/core.c
+++ trunk/src/core.c
@@ -2,47 +2,83 @@
 #include "../inc/core.h"
 #include <pthread.h>
 #include <stdio.h>
+#include <unistd.h>
+
 
 #define CUADRAS 6
+#define TILES_CUADRAS 2
 #define XDIM 3*CUADRAS
 #define YDIM 3*CUADRAS
 #define TRUE 1
 #define FALSE 0
+#define V_GREEN 1
+#define V_RED	0
+#define	H_GREEN V_RED
+#define	V_GREEN H_RED
 
 boolean tiles[YDIM][XDIM];
 point_t buses[XDIM*YDIM];
+semaphore semps[(CUADRAS+1)*(CUADRAS+1)-4];
+int semps_hash[(CUADRAS+1)][(CUADRAS+1)];
 int next_id=0;
-pthread_mutex_t next_id_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t tiles_mutex[YDIM][XDIM] = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t semaphore_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+
+
+
+
 
 int
 main(void) {
-  
+ 
+    
+    while(1){
+	usleep(1000);
+	pthread_mutex_lock(&semaphore_mutex);
+	for(i = 0; i<(CUADRAS+1)*(CUADRAS+1) - 4 ; i++)
+	    switch_semaphore(&semps[i]);
+	pthread_mutex_unlock(&semaphore_mutex);
+
+    }
+    
 }
 
 int
 init(void) {
     
     int i,j;
+    point_t aux;
+    int index = 0;
+    
     
     /* LLeno la matriz de la ciudad, donde hay una manzana pongo TRUE, asi siempre
     esta ocupada y no se puede pasar por encima de una manzana. */
     for(i=0 ; i < YDIM ; i++){
         for(j=0 ; j < XDIM ; j++){
-            if(((i%(CUADRAS/2) == 1) || (i%(CUADRAS/2) == 2)) 
-                && ((j%(CUADRAS/2) == 1) || (j%(CUADRAS/2) == 2)))
+	    aux.x = j;
+	    aux.y = i;
+	    if(hasSemaphore(aux)) {
+		semps[index].state = V_RED;
+		semps_hash[aux.y][aux.x] = index;
+		semps[index++].pos = aux;
+	    }
+            else if(((i%(TILES_CUADRAS+1) == 1) || (i%(TILES_CUADRAS+1) == 2)) 
+                && ((j%(TILES_CUADRAS+1) == 1) || (j%(TILES_CUADRAS+1) == 2)))
                 tiles[i][j] = TRUE;
             else
                 tiles[i][j] = FALSE;
         }
     }
+
     return 0;
 }
 
 void *
 listen() {
     while(1){
-	recieve
+
+	
+
     }
 }
 
@@ -56,8 +92,8 @@ insert_bus(int * id, point_t pos){
     if(!valid_pos(pos)){
         pthread_exit((void *)  NEW_POS_INVALID);
     }
-    if(((pos.y%(CUADRAS/2) == 1) || (pos.y%(CUADRAS/2) == 2)) 
-      && ((pos.x%(CUADRAS/2) == 1) || (pos.x%(CUADRAS/2) == 2))){
+    if(((pos.y%(TILES_CUADRAS+1) == 1) || (pos.y%(TILES_CUADRAS+1) == 2)) 
+      && ((pos.x%(TILES_CUADRAS+1) == 1) || (pos.x%(TILES_CUADRAS+1) == 2))){
         printf("No se puede insertar un colectivo en medio de una manzana.\n");
         return BLOCKED_SLOT;
     }
@@ -73,7 +109,7 @@ insert_bus(int * id, point_t pos){
     buses[next_id] = pos;
     (*id) = next_id;
     next_id++;
-//     pthread_mutex_unlock(&instert_bus_mutex);
+//    pthread_mutex_unlock(&instert_bus_mutex);
     return aux_id;
         
 }
@@ -89,16 +125,22 @@ move_bus(int id, point_t new_pos){
         printf("No te podes mover mas de 1 lugar por turno.\n");
        return NEW_POS_FAR_AWAY;
     }
-    if(tile[new_pos.y][new_pos.x]){
+    pthread_mutex_lock(&semaphore_mutex);
+    if(hassSemaphore(new_pos) && isVRedHGreen(semps[(semps_hash[pos.y][pos.x])]) 
+	&& actual_pos.x == new_pos.x) {
+	printf("No se puede avanzar, semaforo en rojo.\n");
+        return RED_LIGHT_ON;
+    }
+    pthread_mutex_unlock(&semaphore_mutex);
+    if(tile[new_pos.y][new_pos.x] == TRUE){
         printf("No se puede avanzar, nueva posicion esta ocupada.\n");
         return NEW_POS_ALREADY_OCCUPIED;
     }
-    /* aca habria que chequear que esto sea atomico */
-//     pthread_mutex_lock(&tiles_mutex[actual_pos.y][actual_pos.x]);
+
+	
     bus[id] = new_pos;
     tiles[actual_pos.y][actual_pos.x] = FALSE;
     tiles[new_pos.y][new_pos.x] = TRUE;
-//     pthread_mutex_unlock(&tiles_mutex[actual_pos.y][actual_pos.x]);
     return id;
 }
 
@@ -112,6 +154,28 @@ valid_pos(point_t pos){
     }
     return TRUE;
 }
-            
+
+int
+hassSemaphore(point_t pos){
+    if( !((pos.y == 0 || pos.y == CUADRAS*TILES_CUADRAS+1) 
+	&& (pos.X == 0 || pos.X == CUADRAS*TILES_CUADRAS+1))
+	&& ((pos.y % TILES_CUADRAS == 0) && (pos.x % TILES_CUADRAS == 0)))
+	    return 1;
+    return 0;
+}
+
+int
+isVRedHGreen(semaphore s){
+    if( s.state == V_RED ){
+	return TRUE;
+    }
+    return FALSE;
+}
+
+void
+switch_semaphore(sempahore * sem){
+    sem->state = !sem->state;
+}
+
             
          
