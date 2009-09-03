53983157e1a08b291cc0dc918942baa58196f2cb
tecuraotemata <tecuraotemata@e873271c-9239-11de-a307-bbee0a36ac66>
2009-09-03 19:01:23 +0000

    commiteo algo mas

--- trunk/src/marshall.c
+++ trunk/src/marshall.c
@@ -1,15 +1,15 @@
 #include "../inc/typedef.h"
+#include include <dirent.h>
 
 #define CD_NO 0
 #define CD_INSERT_BUS 1
 #define CD_MOVE_BUS
 
-void
-init()
+typedef struct
 {
-    file = w_open();
-}
-
+    int cantFiles;
+    session_t * filesId;
+} files_t;
 
 void
 insert_request(int * id, point_t pos)
@@ -43,6 +43,45 @@ receive(package_t data)
 	case CD_INSERT_BUS: insert_bus(data.integer_p, data.point);
 	case CD_MOVE_BUS: move_bus*
 
+}
+
+
+/*
+** openFiles recibe un char* con el directorio y retorna un puntero a files_t en el cual 
+** contiene un int con la cantidad de archivos que abrio (porque los abre)
+** y un string de session_t con todos los fd de los archivos abiertos que devolvio w_open
+*/
+
+files_t *
+openFiles(char * dir)
+{
+    DIR * direct;
+    struct dirent *opdir;
+    files_t * files = malloc(sizeof(files_t));
+    int i = 0;
+    int fd;
+
+    if( (direct = opendir(dir)) == NULL )
+	printf("Error de directorio\n");
+
+    files->cantFiles = 0;
+    files->filesId = NULL;
+
+    while( (opdir = readdir( direct )) != NULL )
+    {
+	fd = w_open( opdir->d_name );
+	
+	files->cantFiles++;
+	
+	if(i%5==0)
+	    files->filesId=realloc((i+5)*sizeof(int));
+	
+	files->filesId[i] = fd;
+    }
+
+    files->filesId=realloc(i*sizeof(int));
+    
+    return files;
 }
 
 
