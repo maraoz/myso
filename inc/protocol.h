61ea107db1cb7d9dd40325bde99214c62463f2cc
manuelaraoz <manuelaraoz@e873271c-9239-11de-a307-bbee0a36ac66>
2009-09-09 18:21:10 +0000

    update de protocol|

---
+++ inc/protocol.h
@@ -0,0 +1,18 @@
+
+#ifndef _PROTOCOL_H
+#define _PROTOCOL_H
+
+
+#define TRUE 1
+#define FALSE 0
+
+#define SHARED_MEMORY 0
+#define FIFO 1
+#define SOCKET 2
+#define MESSAGE_QUEUE 3
+
+#define CORE 0
+#define LINE 1
+
+
+#endif
