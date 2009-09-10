19b4103d261125f132ec59d5d8cd9a6ee15a1d45
manuelaraoz <manuelaraoz@e873271c-9239-11de-a307-bbee0a36ac66>
2009-09-10 20:58:20 +0000

    agregado de funciones utiles

--- src/util.c
+++ src/util.c
@@ -5,3 +5,29 @@ float
 dist(point_t a, point_t b){
     return sqrt((a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y));
 }
+
+
+int digit_count(int n) {
+    int i;
+    if (n == 0)
+        return 1;
+    for (i=0; n != 0; i++) {
+        n /= 10;
+    }
+    return i;
+}
+
+char * itoa( int n) {
+    char * ret = calloc(11,sizeof(char));
+    if (n == 0) {
+        ret[0] = '0';
+        return ret;
+    }
+    int i;
+    int dc = digit_count(n);
+    for (i=0; n != 0; i++) {
+        ret[dc-i-1] = n % 10 + '0';
+        n /= 10;
+    }
+    return ret;
+}
