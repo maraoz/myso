4aedcf3865c78b9fa9185d5665fe076d087d44b8
williams.matias <williams.matias@e873271c-9239-11de-a307-bbee0a36ac66>
2009-09-12 00:29:32 +0000

    Funcionaaaaaaaaaaaa con 2 files :D

--- trunk/src/core.c
+++ trunk/src/core.c
@@ -137,7 +137,7 @@ move_bus(int fd, int id, point_t new_pos){
     }
     if(hasSemaphore(new_pos) && !isVRedHGreen(semps[(semps_hash[new_pos.y][new_pos.x])]) 
     && actual_pos.y == new_pos.y) {
-        printf("No se puede avanzar, semaforo en rojo.\n");
+     //   printf("No se puede avanzar, semaforo en rojo.\n");
         return RED_LIGHT_ON;
     }
     //pthread_mutex_unlock(&semaphore_mutex);
@@ -147,25 +147,25 @@ move_bus(int fd, int id, point_t new_pos){
         return NEW_POS_ALREADY_OCCUPIED;
     }
 
-    if((aux=(new_pos.x - actual_pos.x)) != 0) {
-        if(new_pos.y%6==0  && aux != 1) {
-            printf("CONTRAMANO.\n");
-            return WRONG_WAY;
-        }
-        if((new_pos.y%3==0 || new_pos.y==XDIM-1) && aux != -1  && new_pos.y != 0) {
-            printf("CONTRAMANO.\n");
-            return WRONG_WAY;
-        }
-    } else if((aux=(new_pos.y - actual_pos.y)) != 0) {
-        if((new_pos.x%6==0 || new_pos.x==YDIM-1 ) && aux != -1) {
-            printf("CONTRAMANO.\n");
-            return WRONG_WAY;
-        }
-        if(new_pos.x%3==0 && aux != 1 && new_pos.x != 0) {
-            printf("CONTRAMANO.\n");
-            return WRONG_WAY;
-        }
-    }
+//     if((aux=(new_pos.x - actual_pos.x)) != 0) {
+//         if(new_pos.y%6==0  && aux != 1) {
+//             printf("CONTRAMANO.\n");
+//             return WRONG_WAY;
+//         }
+//         if((new_pos.y%3==0 || new_pos.y==XDIM-1) && aux != -1  && new_pos.y != 0) {
+//             printf("CONTRAMANO.\n");
+//             return WRONG_WAY;
+//         }
+//     } else if((aux=(new_pos.y - actual_pos.y)) != 0) {
+//         if((new_pos.x%6==0 || new_pos.x==YDIM-1 ) && aux != -1) {
+//             printf("CONTRAMANO.\n");
+//             return WRONG_WAY;
+//         }
+//         if(new_pos.x%3==0 && aux != 1 && new_pos.x != 0) {
+//             printf("CONTRAMANO.\n");
+//             return WRONG_WAY;
+//         }
+//     }
 	
     buses[fd][id] = new_pos;
     pthread_mutex_lock(&map_mutex);
